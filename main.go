a4d9d12716a494f6b0dadd8e5d38828ffd2208ca
zerosatin121 <mathu.muchoki@gmail.com>
2025-08-03 02:00:58 +0300

    added other paths to audit , hunting nand log.

--- main.go
+++ main.go
@@ -18,6 +18,9 @@ func main() {
     tmpl := template.Must(template.ParseFiles(
         filepath.Join("templates", "index.html"),
         filepath.Join("templates", "about.html"),
+        filepath.Join("templates", "audit.html"),
+        filepath.Join("templates", "bugs.html"),
+        filepath.Join("templates", "log.html"),
     ))
     router.SetHTMLTemplate(tmpl)
 
@@ -30,6 +33,21 @@ func main() {
     router.GET("/about", func(c *gin.Context) {
         c.HTML(http.StatusOK, "about.html", nil)
     })
+// Route: Audit challenge page
+    router.GET("/audit-challenge", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "audit.html", nil)
+})
+
+//route to bug hunting page
+router.GET("/bug-hunting", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "bugs.html", nil)
+})
+
+// Route: Daily log page
+router.GET("/daily-log", func(c *gin.Context) {
+    c.HTML(http.StatusOK, "log.html", nil)
+})
+
 
     // Start server
     router.Run(":8080") // Gin logs each request automatically
