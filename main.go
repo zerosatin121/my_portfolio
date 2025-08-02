4415b9e867390651d1de1cc50205d1621f12a4eb
zerosatin121 <mathu.muchoki@gmail.com>
2025-08-03 01:08:10 +0300

    "Initial commit: setup Gin server and index.html routing"

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-    "net/http"
+    "github.com/gin-gonic/gin"
     "html/template"
+    "net/http"
+    "path/filepath"
 )
 
 func main() {
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+    // Create Gin router with default middleware (Logger + Recovery)
+    router := gin.Default()
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        tmpl := template.Must(template.ParseFiles("templates/index.html"))
-        tmpl.Execute(w, nil)
+    // Serve static files from ./static at /static
+    router.Static("/static", "./static")
+
+    // Load templates
+    tmpl := template.Must(template.ParseFiles(
+        filepath.Join("templates", "index.html"),
+        filepath.Join("templates", "about.html"),
+    ))
+    router.SetHTMLTemplate(tmpl)
+
+    // Route: Landing page
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", nil)
     })
 
-    println("Server running at http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+    // Route: About page
+    router.GET("/about", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "about.html", nil)
+    })
+
+    // Start server
+    router.Run(":8080") // Gin logs each request automatically
 }
